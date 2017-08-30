b28ee60edd7b1513b7ed8d22f797d01e025bbc05
byllgrim <robin.a.t.pedersen@gmail.com>
2017-08-30 20:00:49 +0200

    find servers by directory and no '^.'

--- srvctl.adb
+++ srvctl.adb
@@ -13,14 +13,22 @@ package body SrvCtl is
 	procedure Reconnect_Servers is
 		Search : AD.Search_Type;
 		Dir_Ent : AD.Directory_Entry_Type;
+		Kind : AD.File_Kind;
 	begin
 		AD.Start_Search (Search, ".", "");
 		-- TODO don't assume "." is correct dir?
 
 		while AD.More_Entries (Search) loop
 			AD.Get_Next_Entry (Search, Dir_Ent);
+
 			-- TODO check directory or file
-			Maintain_Connection (AD.Simple_Name(Dir_Ent));
+			Kind := AD.Kind (Dir_Ent);
+			if AD.File_Kind'Pos (Kind)
+			   = AD.File_Kind'Pos (AD.Directory) then
+				--TODO define = operator
+				--TODO move to Maintain_Connection
+				Maintain_Connection (AD.Simple_Name(Dir_Ent));
+			end if;
 		end loop;
 
 		AD.End_Search (Search);
@@ -29,6 +37,19 @@ package body SrvCtl is
 	procedure Maintain_Connection (Name : in String) is
 		-- TODO get Dir_Ent as input?
 	begin
+		--TODO check name
+		if Name(Name'First) = '.' then
+			return;
+		end if;
+
+		--Kind := AD.Kind (Dir_Ent);
+		--if AD.File_Kind'Pos (Kind)
+		--   = AD.File_Kind'Pos (AD.Special_File) then
+		--	--TODO define = operator
+		--	--TODO move to Maintain_Connection
+		--	Maintain_Connection (AD.Simple_Name(Dir_Ent));
+		--end if;
+
 		if not Is_Up (Name) then
 			ATIO.Put_Line ("Fifo not up");
 			-- TODO Spawn_Client (Name);
@@ -45,7 +66,7 @@ package body SrvCtl is
 		Relative_Path := ASU.To_Unbounded_String (Name);
 		ASU.Append (Relative_Path, "/in");
 
-		ATIO.Put_Line ("Chec " & ASU.To_String (Relative_Path));
+		ATIO.Put_Line ("Chk " & ASU.To_String (Relative_Path));
 
 		return True; -- TODO open file, check fifo?, check ENXIO
 	end Is_Up;
