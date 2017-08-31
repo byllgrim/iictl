6c64cd8b878055fdad72b3892b8815c136dd2ad1
byllgrim <robin.a.t.pedersen@gmail.com>
2017-08-31 16:50:05 +0200

    separate concern regarding server dir validity checking

--- srvctl.adb
+++ srvctl.adb
@@ -31,10 +31,6 @@ package body SrvCtl is
     procedure Maintain_Connection (Name : in String) is
         -- TODO get Dir_Ent as input?
     begin
-        --TODO check name
-        if Name(Name'First) = '.' then
-            return;
-        end if;
 
         --Kind := AD.Kind (Dir_Ent);
         --if AD.File_Kind'Pos (Kind)
@@ -45,10 +41,11 @@ package body SrvCtl is
         --end if;
 
         if not Is_Up (Name) then
-            ATIO.Put_Line ("Fifo not up");
+            ATIO.Put_Line ("Spawning client for " & Name);
             -- TODO Spawn_Client (Name);
+        else
+            ATIO.Put_Line (Name & " is running"); --TODO remove
         end if;
-        ATIO.Put_Line ("Mainting " & Name); -- TODO remove
     end Maintain_Connection;
 
     function Is_Up (Name : in String) return Boolean is
@@ -60,20 +57,22 @@ package body SrvCtl is
         Relative_Path := ASU.To_Unbounded_String (Name);
         ASU.Append (Relative_Path, "/in");
 
-        ATIO.Put_Line ("Chk " & ASU.To_String (Relative_Path));
+        ATIO.Put_Line ("Isup? " & ASU.To_String (Relative_Path));
 
-        return True; -- TODO open file, check fifo?, check ENXIO
+        return False; -- TODO open file, check fifo?, check ENXIO
     end Is_Up;
 
     function Is_Srv_Dir (Dir_Ent : AD.Directory_Entry_Type) return Boolean is
-        Kind : AD.File_Kind;
+        Kind : AD.File_Kind := AD.Kind (Dir_Ent); --TODO use without variable
+        Name : String := AD.Simple_Name (Dir_Ent);
     begin
-        Kind := AD.Kind (Dir_Ent); --TODO use without variable
+
         if AD.File_Kind'Pos (Kind) /= AD.File_Kind'Pos (AD.Directory) then
             --TODO define = operator
             --TODO move to Maintain_Connection?
             return False;
-        --TODO else if ".." etc
+        elsif Name(Name'First) = '.' then
+            return False;
         --TODO else if no */in */out
         else
             return True;
