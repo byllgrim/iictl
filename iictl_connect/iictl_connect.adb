447782ca82a13b2d25e7f8106841ad937f72a217
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-17 19:10:30 +0100

    [connect] take ii directory from commandline

--- iictl_connect/iictl_connect.adb
+++ iictl_connect/iictl_connect.adb
@@ -1,6 +1,16 @@
-with Ada.Text_IO;
+with Ada.Command_Line;
+with Ada.Strings.Unbounded;
 
 procedure Iictl_Connect is
+   use Ada.Command_Line;
+   use Ada.Strings.Unbounded;
+   Dir : Unbounded_String := To_Unbounded_String (".");
 begin
-   Ada.Text_IO.Put_Line ("Hello, World!");
+   if Argument_Count = 1 then
+      -- TODO allow multiple directories?
+      Dir := To_Unbounded_String (Argument(1));
+      -- TODO use normal String
+   end if;
+
+   -- TODO connect to all servers in Dir
 end Iictl_Connect;
