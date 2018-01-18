f02336f3aeb2f1e58a45c5140c150b18a6ef9bde
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-18 09:23:35 +0100

    enable gnatgcc warnings and optimization

--- prog.mk
+++ prog.mk
@@ -4,6 +4,7 @@
 OBJ = $(SRC:.adb=.o)
 IPATH = /usr/share/ada/adainclude/florist
 OPATH = /usr/lib/i386-linux-gnu/ada/adalib/florist
+FLAGS = -Os -Wall -Wextra
 
 all: $(PROG)
 
@@ -12,7 +13,7 @@ $(PROG): $(OBJ)
 	gnatlink $(PROG) -lflorist -static
 
 .adb.o:
-	gnatgcc -c $< -I$(IPATH)
+	gnatgcc -c $< -I$(IPATH) $(FLAGS)
 
 clean:
 	rm -rf $(PROG) *.o *.ali
